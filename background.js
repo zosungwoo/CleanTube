7b6a2e1c7b635dc8637c580343b72cce5dcb7053
Sungwoo Cho <seouk77@gmail.com>
2023-02-04 16:01:25 +0900

    Fix code before release

--- background.js
+++ background.js
@@ -1,36 +1,20 @@
+async function runContentScript(details) {
+    let queryOptions = { active: true, currentWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    if (details.url.startsWith("https://www.youtube.com/")) {
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            files: ["content.js"]
+        });
+    }
+}
+
 // Listen for tab activation
 chrome.tabs.onActivated.addListener(function(activeInfo) {
-  chrome.tabs.get(activeInfo.tabId, function(tab) {
-    if (tab.url.startsWith("https://www.youtube.com/")) {
-        async function getCurrentTab() {
-            let queryOptions = { active: true, currentWindow: true };
-            let [tab] = await chrome.tabs.query(queryOptions);
-            return tab;
-        }
-        (async () => {
-            let tab = await getCurrentTab();
-            chrome.scripting.executeScript({
-                target: {tabId: tab.id},
-                files: ["content.js"]
-            });
-        })();
-    }
-  });
+    chrome.tabs.get(activeInfo.tabId, function(tab) {
+        runContentScript({ url: tab.url });
+    });
 });
+
 // Listen for when the DOM content has loaded on the current page
-chrome.webNavigation.onDOMContentLoaded.addListener(function(details) {
-    if (details.url.indexOf("https://www.youtube.com/") === 0) {
-        async function getCurrentTab() {
-            let queryOptions = { active: true, currentWindow: true };
-            let [tab] = await chrome.tabs.query(queryOptions);
-            return tab;
-        }
-        (async () => {
-            let tab = await getCurrentTab();
-            chrome.scripting.executeScript({
-                target: {tabId: tab.id},
-                files: ["content.js"]
-            });
-        })();
-    }
-});
+chrome.webNavigation.onDOMContentLoaded.addListener(runContentScript);
